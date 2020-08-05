839ccd5642b3aab294791d09383cb4d3912ec985
Dev Patel <1909devpatel@gmail.com>
2020-08-05 20:30:07 +0530

    made folders for easy finding of files

--- Mono Board pygame.py
+++ Mono Board pygame.py
@@ -1,29 +1,24 @@
-import pygame
-import keyboard,os,random
+import pygame, keyboard,os,random
+from information import *
 
+# to set the width and height to fit our screen
 os.environ['SDL_VIDEO_CENTERED'] = '1' # You have to call this before pygame.init()
 
 pygame.init()
 
-info = pygame.display.Info() # You have to call this before pygame.display.set_mode()
+info = pygame.display.Info()
 screen_width,screen_height = info.current_w,info.current_h
 
-grey = (211,211,211)
-brown = (139,69,19)
-black = (0,0,0)
-white = (255,255,255)
-green = (0,255,0)
-
 gameDisplay = pygame.display.set_mode((screen_width,screen_height))
-gameDisplay.fill(grey)
 
+# will run the main loop
 run = True
-
 
 board_image = pygame.image.load('monopoly board.jpg')
 board_image = pygame.transform.scale(board_image, (screen_width,screen_height-20))
 gameDisplay.blit(board_image,(0,0))
 
+# a dictioanry including all the places on the board!!!
 my_places ={'go_box':" ",'mediteranean_avenue':" ",'community1' :" ",'baltic_avenue':" ",'income_tax':" ",
             'reading_railroad' :" ",'oriental_avenue':" ",'chance1':" ",'vermount_avenue':" "
             ,'connecticut_avenue':" ",'just_visiting':" "
@@ -34,31 +29,49 @@ my_places ={'go_box':" ",'mediteranean_avenue':" ",'community1' :" ",'baltic_ave
         'pacific_avenue':" ",'north_carolina_avenue':" ",'community3':" ",'pennsylvania_avenue':" ",
             'shortline':" ",'chance3':" ",'park_place':" ",'luxury_tax':" ",'board_walk':" "}
 
-row_coordinates= {}
-column_coordinates = {}
-myplace_num ={}
-myplaces=[]
+#in this dictionary we will add pixel coordinates of eachplace on the board!
+pixels_cords= {}
 
+#this dictionary will contain a unique identity no. for each place on the board
+# which is the no. of the places from go box with go box being 0
+myplace_id ={}
+
+# to
+id = 0
+for place in my_places.keys():
+    myplace_id.update({place:id})
+    id += 1
+# every token starts form the go box thus initial positon = 0
+position = 0
+
+# for the purpose of storing our places in a list (might be of some help in future)
+myplaces=[]
 for place in my_places.keys():
     myplaces.append(str(place))
 
+#to quit the game and make run = False so we stop our main loop of game
 def leave():
     global run
     pygame.quit()
     run = False
 
+# to make a function which we will use to make rectangles for our tokens easily.
 def things(thingx, thingy, thingw, thingh, color):
     pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
-#position = (1307,721)
-token1 = things(1307,700,30,40,white)
 
-#def show_a_button()
-position = 0
+# we call the above defined function to make a rectangle
+token1 = things(1307,700,30,40,blue)
+
+#to show_a_button()
+
 def roll_dice():
     global position
+    # we random a no. to simulate dice roll
     dice_roll = random.randint(0,6)
+    # adding the the dice roll to position
     position += dice_roll
-    if position>=41:
+    # if postion exceeds 40 (which is the last position onn our board) then we
+    if position >= 41:
         position -= 41
 
 while run:
