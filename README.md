3400f4f4c7e7a6ed75bf210dfeaa2f891a9d987e
Dev Patel <1909devpatel@gmail.com>
2020-08-18 19:36:41 +0530

    making classes

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ still in progress(see code)
 ## pygame
 Decided not to use pygame! All the pygame files have been moved to the using_pygame folder.
 ## New Branch-Token Movement
-A moonopoly game consists of many tokens not one.
+A monopoly game consists of many tokens not one.
 So this brach will be used to develop a 
 coordination between multiple tokens!
--- initialising_everything.py
+++ initialising_everything.py
@@ -1,3 +1,46 @@
 master_dictionary = {"tokens":{},"players":{}}
 playing_tokens = []
-# dump everything in master dictionary when going to make save feature
+position = 0
+
+
+"""
+import tkinter
+root=tkinter.Tk()# dump everything in master dictionary when going to make save feature
+width = root.winfo_screenwidth() #width of screen
+height = root.winfo_screenheight() # height of screen
+print(width)
+print(height)
+root.geometry("%dx%d%+d%+d" % (width, height, 0 ,0))
+frame = tkinter.Frame(root,width=200,height=150,bg='blue')
+frame.grid(row=0,column=0)
+label = tkinter.Label(root,text="T1!",width=2,height=1)
+b = tkinter.Label(root,text="T1!",width=2,height=1)
+a = tkinter.Label(root,text="T1!",width=2,height=1)
+label.place(in_=frame,relx=0,rely=0,anchor="s")
+a.place(in_=frame,relx=0,rely=0,anchor="s")
+b.place(in_=frame,relx=0,rely=0,anchor="w")
+root.mainloop()
+tkinter.Label(root,text="590,285").place(relx=0.5,rely=0.5,x=590,y=285)
+tkinter.Label(root,text="450,285").place(relx=0.5,rely=0.5,x=450,y=285)
+tkinter.Label(root,text="330,285").place(relx=0.5,rely=0.5,x=330,y=285)
+tkinter.Label(root,text="210,285").place(relx=0.5,rely=0.5,x=210,y=285)
+tkinter.Label(root,text="90,285").place(relx=0.5,rely=0.5,x=100,y=285)
+tkinter.Label(root,text="-500,285").place(relx=0.5,rely=0.5,x=-500,y=285)
+tkinter.Label(root,text="-640,285").place(relx=0.5,rely=0.5,x=-640,y=285)
+
+tkinter.Label(root,text="-640,180").place(relx=0.5,rely=0.5,x=-640,y=180)
+tkinter.Label(root,text="-640,140").place(relx=0.5,rely=0.5,x=-640,y=140)
+tkinter.Label(root,text="-640,90").place(relx=0.5,rely=0.5,x=-640,y=90)
+tkinter.Label(root,text="-640,40").place(relx=0.5,rely=0.5,x=-640,y=40)
+tkinter.Label(root,text="-10").place(relx=0.5,rely=0.5,x=-640,y=-10)
+tkinter.Label(root,text="-640,-220").place(relx=0.5,rely=0.5,x=-640,y=-210)
+tkinter.Label(root,text="-640,-300").place(relx=0.5,rely=0.5,x=-640,y=-300)
+
+tkinter.Label(root,text="-500,-300").place(relx=0.5,rely=0.5,x=-500,y=-300)
+tkinter.Label(root,text="-380,-300").place(relx=0.5,rely=0.5,x=-380,y=-300)
+tkinter.Label(root,text="-260,-300").place(relx=0.5,rely=0.5,x=-260,y=-300)
+
+tkinter.Label(root,text="580,-300").place(relx=0.5,rely=0.5,x=580,y=-300)
+tkinter.Label(root,text="580,-205").place(relx=0.5,rely=0.5,x=580,y=-205)
+tkinter.Label(root,text="580,-10").place(relx=0.5,rely=0.5,x=580,y=-10)
+tkinter.Label(root,text="580,-155").place(relx=0.5,rely=0.5,x=580,y=-155)"""
