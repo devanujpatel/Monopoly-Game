4171971909808fe70ac1659ed91014357c408b34
devanujpatel <1909devpatel@gmail.com>
2020-09-23 22:21:14 +0530

    create room and join room done!

--- client.py
+++ client.py
@@ -2,8 +2,8 @@ import socket
 import tkinter as tk
 
 client = socket.socket()
-client.connect(("127.0.01",9999))
-
+client.connect(("192.168.29.201",9999))
+HEADER = 10
 container = tk.Tk()
 
 # setting width and height of tkinter window so as to fit screen!
@@ -16,25 +16,57 @@ start_frame= tk.Frame(container, width=width, height=height)
 start_frame.grid(row=0, column=0, sticky="nsew")
 
 # make a grid on the container so as to make placing easier
-
-for row in range(5):
-    for col in range(5):
-        tk.Frame(start_frame).grid(row= row, colmun = col)
+tk.Frame(start_frame,width= width/7,height=height/7).grid(row=0,column=0)
+tk.Frame(start_frame,width= width/7,height=height/7).grid(row=0,column=1)
+tk.Frame(start_frame,width= width/7,height=height/7).grid(row=0,column=2)
+tk.Frame(start_frame,width= width/7,height=height/7).grid(row=0,column=3)
+tk.Frame(start_frame,width= width/7,height=height/7).grid(row=0,column=4)
 
 font = ("Courier", 13)
 username_entry = tk.Entry(start_frame)
-ok_but_for_username = tk.Button(start_frame, text= "Okay",font= font,command= lambda :ok_but_for_username_clicked)
+ok_but_for_username = tk.Button(start_frame, text= "Okay",font= font,command= lambda :ok_but_for_username_clicked())
 username_entry.grid(row=3,column=3)
 ok_but_for_username.grid(row=4,column=3)
 
+def create_room():
+    join_room_btn.grid_forget()
+    create_room_btn.grid_forget()
+    print("sending to create new room!")
+    client.send(bytes("1",'utf-8'))
+
+def join_room():
+    join_room_btn.grid_forget()
+    create_room_btn.grid_forget()
+    print("sending to join room!")
+    client.send(bytes("0", 'utf-8'))
+    global room_num_entry, ok_but_room_num
+    room_num_entry = tk.Entry(start_frame)
+    room_num_entry.grid(row=2,column=3)
+    ok_but_room_num = tk.Button(start_frame, text="Ok",command = lambda:ok_but_room_num_clkd())
+    ok_but_room_num.grid(row=2,column=4)
 def ok_but_for_username_clicked():
     ok_but_for_username.grid_forget()
-    username = username_entry.get()
+    username_entry.grid_forget()
+    username = str(username_entry.get())
 
-    if username == "":
-        ok_but_for_username.grid(row=4,column=3)
+    print("sending username")
+    username_sendable = f"{len(username):<{HEADER}}"+username
+    client.send(bytes(username_sendable,'utf-8'))
 
-    else:
-        username_entry.grid_forget()
+    global create_room_btn, join_room_btn
+    create_room_btn = tk.Button(start_frame,text="Create Room",command=lambda:create_room())
+    create_room_btn.grid(row=3,column=2)
+    join_room_btn = tk.Button(start_frame, text="Join Room", command=lambda: join_room())
+    join_room_btn.grid(row=3, column=4)
 
-    client.send(bytes(username,'utf-8'))
+def ok_but_room_num_clkd():
+    room_num_entry.grid_forget()
+    ok_but_room_num.grid_forget()
+
+    room = room_num_entry.get()
+    client.send(bytes(room,'utf-8'))
+
+
+
+
+container.mainloop()
--- server.py
+++ server.py
@@ -1,88 +1,77 @@
-import socket
-import threading
-import pickle
-import select
+import socket, threading, pickle
 
-# create our server socket : defaults to AF_INET and streaming socket!
+# create a af_inet streaming server
 server = socket.socket()
-server.bind(("",9999))
+server.bind(("", 9999))
 server.listen()
 
 HEADER = 10
-sockets_list = [server]
 
-# dictionary that handles everythong
+# a dictionary that contains each and every room created and its data about players
 rooms = {}
+# start making rooms with no. 100
 room = 100
 
-# start new thread for recieving username and etc. stuff (this will also be the thread which will handle the game for the player)
-class client_class(threading.Thread):
-    def __init__(self, client, addr,read_socks):
+
+class threaded_Client(threading.Thread):
+    def __init__(self, client, addr):
         threading.Thread.__init__(self)
         self.client = client
         self.addr = addr
-        self.read_socks = read_socks
 
     def run(self):
-        # recv username , new room or join room , favourite color, etc...
-        # do further things like waiting or playing
-        global room
-        # get length of usernmame and then buffer the whole username
-        user_name_length = self.client.recv(HEADER)
-        user_name_length = int(user_name_length.decode('utf-8'))
-        username = self.client.recv(user_name_length)
+        try:
+            global rooms, room
+            print("Running: " + threading.Thread.getName(self))
+            print("waiting for client", self.addr, "to send username!")
+            # receiving username length to later receive whole username
+            self.username_length = self.client.recv(HEADER).decode('utf-8')
+            self.username_length = int(self.username_length)
+            self.username = str(self.client.recv(self.username_length).decode('utf-8'))
+            print("received username from", self.addr, ":", self.username)
 
-        self.username = username
-        # inform us about the new connection
-        print(f"Connection from {username},{self.client}{self.addr} has been establieshed successfully !!!")
-        # send a msg to client about the same
-        msg_to_client = "Connection established successfully, enjoy your game of monopoly!"
-        self.client.send(bytes(msg_to_client,"utf-8"))
+            print("waiting for client", self.addr, "to send further action msg!")
+            what_to_do = int(self.client.recv(16).decode('utf-8'))
 
-        # recv from the client whether he/she wants to create a room or join a room ! (0 TO CREATE A ROOM AND 1 TO JOIN A ROOM.)
-        what_to_do = self.client.recv(8)
-        what_to_do = what_to_do.decode('utf-8')
+            # wait for client to send whether he/she wants to join a room(0) or create a room(1)
 
-        if what_to_do == 0:
-            self.create_room()
-            # to give the next room created a unique room name
-            room += 1
+            # create a room
+            if what_to_do == 1:
+                print("Client creating a room.")
+                # the rooms dicto will be updated with a room no. and another dicto which includes
+                # names of players and their objects
+                rooms.update({room: {"players": {self.username: self.client}}})
+                print(rooms)
+                room += 1
+                print("next room created will be", str(room))
+                self.client.close()
 
-        if what_to_do == 1:
-            # do something to join a room
-            pass
+            # join a room
+            if what_to_do == 0:
+                print("Client joining a room.")
+                room_num = self.client.recv(16).decode('utf-8')
+                room_num = int(room_num)
+                rooms[room_num]["player names"].append(self.username)
+                rooms[room_num]["player objects"].append(self.client)
+                print(rooms)
 
-    def create_room(self):
-        # create a room
-        # the first item in the players list(which is the value of 'players' key below) will be the host and further players will be added later
-        self.room_dicto = {"players":[self.username]}
+                self.client.close()
 
-        # also add it in the main rooms dictionary
-        rooms.update({room:self.room_dicto})
-
-        # a variable waiting which could be True or False, if False then start game- start game will only run when host tells to do so
-        waiting = self.client.recv(16)
-        # start the game when received (recall about blocking sockets!)
-        # can only send str so "false"
-        if waiting == "false":
-            self.start_game()
-        # may have to do some modifications
-
-
-    def start_game(self):
-        # players should see the main game frame
-
-        self.info = {"player_num":len(self.room_dicto['players']),}
-        self.client.send(bytes("true",'utf-8'))
-
+        except Exception as e:
+            print("an error occured", e)
+            # close the connection (later delete from dictionaries)
+            self.client.close()
 
 
 while True:
     client, addr = server.accept()
-    read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
-    # creating obj of Client hope it overwrites the obj name everytime a new player joins
-    handling_thread = client_class(client, addr,read_sockets)
-    handling_thread.start()
+    print("Accepted connection from", addr)
+    print("creating new thread for", addr)
+    client_thread = threaded_Client(client, addr)
+    client_thread.start()
+    print("Looking for more clients in main thread")
 
-
-
+# TODO
+#   continuously check whether client has disconnected (if disconnected then close)
+#   send info of new room created to the host and to players who joined
+#   improve gui looks
