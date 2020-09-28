b5b7e1cbac2d680255f08aeefc4b79c27b2264ad
devanujpatel <1909devpatel@gmail.com>
2020-09-28 18:53:21 +0530

    create and join room!

--- client.py
+++ client.py
@@ -1,7 +1,6 @@
 import socket, pickle, threading
 import tkinter as tk
 
-
 client = socket.socket()
 client.connect(("192.168.29.201",9999))
 HEADER = 10
@@ -25,7 +24,7 @@ tk.Frame(start_frame,width= width/7,height=height/7).grid(row=0,column=4)
 
 font = ("Courier", 13)
 
-# ask for username
+"""# ask for username
 username_entry = tk.Entry(start_frame)
 ok_but_for_username = tk.Button(start_frame, text= "Okay",font= font,command= lambda :ok_but_for_username_clicked())
 username_entry.grid(row=3,column=3)
@@ -38,13 +37,81 @@ def ok_but_for_username_clicked():
 
     print("sending username")
     username_sendable = f"{len(username):<{HEADER}}"+username
-    client.send(bytes(username_sendable,'utf-8'))
+    client.send(bytes(username_sendable,'utf-8'))"""
 
-    global create_room_btn, join_room_btn
-    create_room_btn = tk.Button(start_frame,text="Create Room",command=lambda:create_room())
-    create_room_btn.grid(row=3,column=2)
-    join_room_btn = tk.Button(start_frame, text="Join Room", command=lambda: join_room())
-    join_room_btn.grid(row=3, column=4)
+# give our client a choice of creating or joining a room 
+create_room_btn = tk.Button(start_frame,text="Create Room",command=lambda:create_room())
+create_room_btn.grid(row=3,column=2)
+join_room_btn = tk.Button(start_frame, text="Join Room", command=lambda: join_room())
+join_room_btn.grid(row=3, column=4)
+
+def create_room():
+    join_room_btn.grid_forget()
+    create_room_btn.grid_forget()
+    print("sending to create new room!")
+    client.send(bytes("create room", 'utf-8'))
+    ask_username()
+    
+def join_room():
+    join_room_btn.grid_forget()
+    create_room_btn.grid_forget()
+    print("sending to join room!")
+    client.send(bytes("join room", 'utf-8'))
+    ask_room_num()
+
+def ask_username():
+    # ask for username
+    global ok_but_for_username, username_entry
+    username_entry = tk.Entry(start_frame)
+    ok_but_for_username = tk.Button(start_frame, text="Okay", font=font, command=lambda: ok_but_for_username_clicked())
+    username_entry.grid(row=3, column=3)
+    ok_but_for_username.grid(row=4, column=3)
+
+def ok_but_for_username_clicked():
+    ok_but_for_username.grid_forget()
+    username_entry.grid_forget()
+    username = str(username_entry.get())
+    print("sending username")
+    username_sendable = f"{len(username):<{HEADER}}" + username
+    client.send(bytes(username_sendable, 'utf-8'))
+    start_game_btn = tk.Button(start_frame, text="Start Game",command = lambda:start_game_host())
+    start_game_btn.grid(row=4, column=3)
+    check_on_new_thread_npl = recv_new_players_list_thread()
+    check_on_new_thread_npl.start()
+
+def ask_room_num():
+    global room_num_entry, ok_but_room_num
+    room_num_entry = tk.Entry(start_frame)
+    room_num_entry.grid(row=2,column=3)
+    ok_but_room_num = tk.Button(start_frame, text="Ok",command = lambda:ok_but_room_num_clkd())
+    ok_but_room_num.grid(row=2,column=4)
+
+def ok_but_room_num_clkd():
+    room_num_entry.grid_forget()
+    ok_but_room_num.grid_forget()
+    room = room_num_entry.get()
+    client.send(bytes(room,'utf-8'))
+    status = client.recv(1024).decode('utf-8')
+
+    if str(status) == "joined successfully":
+        # ask and then send the username
+        # ask for username
+        ask_username()
+
+    if str(status) == "room doesn't exist":
+        # ask client to enter a valid room no.
+        print(str(status))
+        ask_room_num()
+
+    if str(status) == "unable to join temp":
+        # ask client to wait for some time and then try to join again (this is the most rare to happen)
+        print("please try to join the room in a few seconds")
+        ask_room_num()
+
+    if str(status) == "room locked":
+        # ask client to enter another room num
+        print("room is either full or locked by the host!")
+        ask_room_num()
 
 class recv_new_players_list_thread(threading.Thread):
     def __init__(self):
@@ -56,60 +123,49 @@ class recv_new_players_list_thread(threading.Thread):
         print("checking for new players list on",threading.Thread.getName(self))
         check_for_new_players_list_stat =  True
         while check_for_new_players_list_stat:
-            new_players_list = client.recv(1024 * 3)
+
+            new_players_list = client.recv(1024)
             if new_players_list:
                 new_players_list = pickle.loads(new_players_list)
-                print("new players list = ", new_players_list)
-            else:
-                pass
+                if new_players_list == "start game":
+                    check_for_new_players_list_stat = False
+                    start_game_player()
+                else:
+                    print("new players list = ", new_players_list)
 
-def create_room():
-    join_room_btn.grid_forget()
-    create_room_btn.grid_forget()
-    print("sending to create new room!")
-    client.send(bytes("1",'utf-8'))
-    print("recving players list")
-    players_in_room = client.recv(1024)
-    players_in_room = pickle.loads(players_in_room)
-    print(players_in_room)
-
-    start_game_btn = tk.Button(start_frame,text= "Start Game",command= lambda: start_game())
-    start_game_btn.grid(row=1, column=1)
-    check_thread = recv_new_players_list_thread()
-    check_thread.start()
-
-def join_room():
-    join_room_btn.grid_forget()
-    create_room_btn.grid_forget()
-    print("sending to join room!")
-    client.send(bytes("0", 'utf-8'))
-    global room_num_entry, ok_but_room_num
-    room_num_entry = tk.Entry(start_frame)
-    room_num_entry.grid(row=2,column=3)
-    ok_but_room_num = tk.Button(start_frame, text="Ok",command = lambda:ok_but_room_num_clkd())
-    ok_but_room_num.grid(row=2,column=4)
-
-def start_game():
+def start_game_host():
+    client.send(bytes("start the game",'utf-8'))
     global check_for_new_players_list_stat
     check_for_new_players_list_stat = False
     print("player started the game")
 
-def ok_but_room_num_clkd():
-    room_num_entry.grid_forget()
-    ok_but_room_num.grid_forget()
+def start_game_player():
+    print("Yo lets play")
 
-    room = room_num_entry.get()
-    client.send(bytes(room,'utf-8'))
-    print("recving players list on new thread")
-    """players_in_room = client.recv(1024)
-    try:
-        print("trying to load new players list")
-        players_in_room = pickle.loads(players_in_room)
-        print(players_in_room)
-    except EOFError as e:
-        print("EOFError!")
-        pass"""
-    check_thread = recv_new_players_list_thread()
-    check_thread.start()
+def recv_game_details():
+    pass
+
+"""def recv_players_list()
+    print("recving players list")
+    players_in_room = client.recv(1024)
+    players_in_room = pickle.loads(players_in_room)
+    print(players_in_room)
+    
+    
+    global ok_but_for_username, username_entry
+        username_entry = tk.Entry(start_frame)
+        ok_but_for_username = tk.Button(start_frame, text="Okay", font=font, command=lambda: ok_but_for_username_clicked())
+        username_entry.grid(row=3, column=3)
+        ok_but_for_username.grid(row=4, column=3)
+        # show the players list
+        # recv for new players list on new thread
+        print("recving players list on new thread")
+        check_thread = recv_new_players_list_thread()
+        check_thread.start()
+        
+        ConnectionResetError: [WinError 10054] An existing connection was forcibly closed by the remote host
+"""
+
+
 
 container.mainloop()
--- server.py
+++ server.py
@@ -9,6 +9,11 @@ HEADER = 10
 
 # a dictionary that contains each and every room created and its data about players
 rooms = {}
+# a dictionary which will contain information which is sent to client many times during the course of the game
+room_game_info = {}
+# a dicto which will contain socket objects of each and every player in a particular room
+room_player_objs = {}
+
 existing_rooms = []
 # start making rooms with no. 100
 room = 100
@@ -23,93 +28,132 @@ class threaded_Client(threading.Thread):
         global rooms, room
         print("Running: " + threading.Thread.getName(self))
         print("waiting for client", self.addr, "to send further action msg!")
-        # wait for client to send whether he/she wants to join a room(0) or create a room(1)
-        what_to_do = int(self.client.recv(16).decode('utf-8'))
+        # wait for client to send whether he/she wants to join a room or create a room
+        what_to_do = (self.client.recv(16).decode('utf-8'))
         print("recved what to do msg from client")
 
         # create a room
-        if what_to_do == 1:
-            
-            # give our host player a room no.
-            self.room = room
-            # add the particular room no. in existing rooms list to later know that the room exists
-            existing_rooms.append(room)
-            # increment for any other player who creates a new room as we want to give new room num to each room created
-            room += 1
-            print("next room created will be", str(room))
-
-            # recv username from the player for that particular game
-            # this is asked inside the if statement and not outside as later a save game feature will be available and 
-            # the player might have to choose who he/she is from the saved room  
-            
-            # receiving username length to later receive whole username
-            self.username_length = self.client.recv(HEADER).decode('utf-8')
-            self.username_length = int(self.username_length)
-            self.username = str(self.client.recv(self.username_length).decode('utf-8'))
-            print("received username from", self.addr, ":", self.username,"for room",room)
-            
-            print(self.username,"is creating a room.")
-            # the rooms dicto will be updated with a room no. and other necessary information
-            rooms.update({room: {"host":self.username,"status":"looking for players","players": {self.username: self.client},"players list":[self.username]}})
-            
-            # send our host the players in his room (at the moment it would be only one player i.e the host itself)
-            self.client.send(pickle.dumps(rooms[self.room]["players list"]))
-
-            # recv a msg to know that host wants to start the game -- recall blocking sockets
-            start_game = self.client.recv(24).decode('utf-8')
-            rooms[self.room]["status"] = "room locked"
-            print("starting the game for room",self.room,"whose host is",self.username)
-            
-            # sending all the players a msg to start the game so they our client side code knows to update the screen
-            for player in rooms[self.room]["players list"]:
-                print("sending to",player)
-                rooms[self.room]["players"][player].send(bytes("start game",'utf-8'))
-            print("successfully sent msg to all the players")
+        if what_to_do == "create room":
+            self.create_room()
 
         # join a room
-        if what_to_do == 0:
-            self.room_num = self.client.recv(16).decode('utf-8')
-            self.room_num = int(self.room_num)
-            if self.room_num not in existing_rooms:
-                # if room doesnt exist then notify the client about the same
-                self.client.send(bytes("room doesnt exist",'utf-8'))
+        if what_to_do == "join room":
+            self.join_room()
 
-            if self.room_num in existing_rooms:
-                self.room = self.room_num
+    def allocate_chance_num(self):
+        # increment chance alloc num by 1
+        rooms[self.room]["chance alloc num"] += 1
+        # return the chance -1 to allocate the chance num to player
+        return rooms[self.room]["chance alloc num"] - 1
+        # one is subtracted while returning above as we added one before
+        # this is so as we need to add one and also return chance num to player
+        # if we wrote return before and increment later then the execution of the function will stop at the return
+        # statement and incrementation of chance num wouldn't happen!
+
+
+    def create_room(self):
+        global rooms, room
+        # give our host player a room no.
+        self.room = room
+        print(self.room)
+        # add the particular room no. in existing rooms list to later know that the room exists
+        existing_rooms.append(self.room)
+        print(existing_rooms)
+        # increment for any other player who creates a new room as we want to give new room num to each room created
+        room += 1
+        print("next room created will be", str(room))
+
+        # recv username from the player for that particular game
+        # this is asked inside the if statement and not outside as later a save game feature will be available and
+        # the player might have to choose who he/she is from the saved room
+
+        # receiving username length to later receive whole username
+        self.username_length = self.client.recv(HEADER).decode('utf-8')
+        self.username_length = int(self.username_length)
+
+        # now recv the username with buffer size = length of username received earlier
+        self.username = str(self.client.recv(self.username_length).decode('utf-8'))
+        print("received username from", self.addr, ":", self.username, "for room", self.room)
+
+        print(self.username, "is creating a room.")
+        # the rooms dicto will be updated with a room no. and other necessary information
+        rooms.update({self.room: {"host": self.username, "status": "looking for players",
+                              "players list": [self.username],"chance alloc num":0}})
+        room_player_objs.update({self.room:{"players": {self.username: self.client}}})
+
+        self.chance = self.allocate_chance_num()
+        print(self.username,str(self. chance))
+        # send our host the players in his room (at the moment it would be only one player i.e the host itself)
+        self.client.send(pickle.dumps(rooms[self.room]["players list"]))
+
+        # recv a msg to know that host wants to start the game -- recall blocking sockets
+        self.start_game = self.client.recv(24)
+        rooms[self.room]["status"] = "room locked temp"
+        print("starting the game for room", self.room, "whose host is", self.username)
+
+        # sending all the players a msg to start the game so our client side code knows to update the screen
+        for player in rooms[self.room]["players list"]:
+            if player != rooms[self.room]["host"]:
+                print("sending to", player)
+                room_player_objs[self.room]["players"][player].send(pickle.dumps("start game"))
+        print("successfully sent msg to all the players")
+        rooms[self.room]["status"] = "looking for players"
+
+    def recv_room_num(self):
+        self.room = self.client.recv(16).decode('utf-8')
+        self.room = int(self.room)
+        self.check_recved_room_num()
+
+    def check_recved_room_num(self):
+        if self.room not in existing_rooms:
+            # if room doesnt exist then notify the client about the same
+            self.client.send(bytes("room doesn't exist", 'utf-8'))
+            self.recv_room_num()
+
+        elif self.room in existing_rooms:
+            if rooms[self.room]["status"] == "looking for players":
+                self.client.send(bytes("joined successfully", 'utf-8'))
+
                 # receiving username length to later receive whole username
                 self.username_length = self.client.recv(HEADER).decode('utf-8')
                 self.username_length = int(self.username_length)
                 self.username = str(self.client.recv(self.username_length).decode('utf-8'))
                 print("received username from", self.addr, ":", self.username, "for room", self.room)
 
-                if rooms[self.room]["status"] == "looking for players":
-                    rooms[self.room]["players"][self.username] = self.client
-                    rooms[self.room]["players list"].append(self.username)
-                    print(rooms)
-                    # self.client.send(pickle.dumps(rooms[self.room]["players list"]))
-                    # send all the players the new list
-                    for player in rooms[self.room]["players list"]:
-                        print(player)
-                        print(rooms[self.room]["players"][player])
-                        rooms[self.room]["players"][player].send(pickle.dumps(rooms[self.room]["players list"]))
+                room_player_objs[self.room]["players"][self.username] = self.client
+                rooms[self.room]["players list"].append(self.username)
+                print(rooms)
+                # self.client.send(pickle.dumps(rooms[self.room]["players list"]))
+                # send all the players the new list
+                for player in rooms[self.room]["players list"]:
+                    room_player_objs[self.room]["players"][player].send(pickle.dumps(rooms[self.room]["players list"]))
 
-                elif rooms[self.room]["status"] == "room locked":
-                    # send the client that room is locked
-                    # locking the room is in the hands of the host but when the host starts the actual game
-                    # then for some seconds the room will be locked
-                    # then again it will be unlocked as it is the default though the host can lock it later
-                    # the room was locked temporarily as the client side code can only recv on thing at a time
-                    # and it would already be looking for new players list incase someone new joins
-                    # whenever someone new joins the new comer sends all the pther players the new updated players list
-                    # but when the host starts the game all the clients need to recv it so the room is lockes temporarily
-                    # so that no new player can join the room and send the new players list when the host is trying to start
-                    # the game ----- this is a precautionary measure this is to handle a situation where the starting of game command
-                    # from the host and joining of a new player doesn't collide so that the client code
-                    # do not gets confused what it has to recv
+            elif rooms[self.room]["status"] == "room locked temp":
+                # send the client that room is locked
+                # locking the room is in the hands of the host but when the host starts the actual game
+                # then for some seconds the room will be locked
+                # then again it will be unlocked as it is the default though the host can lock it later
+                # the room was locked temporarily as the client side code can only recv on thing at a time
+                # and it would already be looking for new players list incase someone new joins
+                # whenever someone new joins the new comer sends all the pther players the new updated players list
+                # but when the host starts the game all the clients need to recv it so the room is lockes temporarily
+                # so that no new player can join the room and send the new players list when the host is trying to start
+                # the game ----- this is a precautionary measure this is to handle a situation where the starting of game command
+                # from the host and joining of a new player doesn't collide so that the client code
+                # do not gets confused what it has to recv
+                self.client.send(bytes("unable to join temp"))
+                # the client side code will then inform our client to try entering the room after a few seconds
+                self.recv_room_num()
+                # new idea here - do a while loop so that the player joins automatically when the room is again unlocked
+                # thus our client doesn't have to send to join room again
+            elif rooms[self.room]["status"] == "room locked":
+                self.client.send(bytes("room locked",'utf-8'))
+                self.recv_room_num()
 
-                    self.client.send(bytes("unable to join"))
-                    # also later join the room when it is unlocked
+            # may add a else here for handling some extreme cases and send clinet: some error occured plz try again
 
+    def join_room(self):
+        self.recv_room_num()
 
 
 while True:
@@ -121,11 +165,14 @@ while True:
     print("Looking for more clients in main thread")
 
 # TODO
-#   give timeouts so that you can reomove a person who is inactive for a long time like 120 seconds
+#   give timeouts so that you can reomove a person who is inactive for a long time like 120 seconds or 3 times inactive for 30 sec
 #   improve gui looks
-"""
-# receiving username length to later receive whole username
-        self.username_length = self.client.recv(HEADER).decode('utf-8')
-        self.username_length = int(self.username_length)
-        self.username = str(self.client.recv(self.username_length).decode('utf-8'))
-        print("received username from", self.addr, ":", self.username)"""
+#   verify username (is it pre occupied or client sent an empty string,etc...)
+#   handle input erros from client side like client sent a str instead of room int
+#   dont allow to start game if only host is in the room and also automatically start game when 8 players join
+
+# ConnectionResetError: [WinError 10054] An existing connection was forcibly closed by the remote host
+# what happens when server sends something but the client didnt recv it as the client has lost connection
+
+# when u send the length of msg using the HEADER technique then recv whole msg then we use recv two times one time
+# on recver side but send once on the sender side????
