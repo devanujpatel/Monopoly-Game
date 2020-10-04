8040224e669cd87217e24f3810fb28ec909bfc13
Dev Patel <1909devpatel@gmail.com>
2020-10-04 20:43:58 +0530

    prop display with full color boxes

--- prop class for online version.py
+++ prop class for online version.py
@@ -29,7 +29,7 @@ class my_property_class:
     def __init__(self, property_str, row, column, width, height, color=None, rent=None, price=None,
                  one_house_rent=None, two_house_rent=None,
                  three_house_rent=None, four_house_rent=None, hotel_rent=None, cost_of_house=None,
-                 cost_of_hotel=None, mortgage_value=None, color_box_side=None, cbw=None, cbh = None):
+                 cost_of_hotel=None, mortgage_value=None, color_box_side=None):
 
         self.property_str = property_str
         self.width = width
@@ -46,21 +46,16 @@ class my_property_class:
         self.rent = rent
         self.cost_of_house = cost_of_house
         self.color_box_side = color_box_side
-        self.cbw = cbw
-        self.cbh = cbh
 
         row_coordinates.update({property_str: row})
         column_coordinates.update({property_str: column})
 
         if self.property_str not in special_properties:
             prop_info.update({self.property_str: {"price": self.price, "houses": 0, "owner": None, "players on site": []}})
-            self.prop_box = tk.Frame(main_frame, width=width, height=height, highlightbackground="black",
+            self.prop_box = tk.Frame(main_frame, width=width, height=height,bg = self.color, highlightbackground="black",
                                      highlightthickness=1)
             self.prop_box.grid(row=row, column=column)
 
-            self.color_box = tk.Frame(main_frame, bg=self.color, highlightbackground="black", highlightthickness=1,
-                                      width=self.cbw, height=self.cbh)
-            self.color_box.grid(row=row, column=column, sticky=self.color_box_side)
             self.buy_button = tk.Button(main_frame, text="Buy", bg=self.color,
                                         command=lambda: self.buy_prop())
 
@@ -169,65 +164,59 @@ height = height / 9
 
 # top lane
 
-w = width
-h = height / 3
-
 free_parking = my_property_class("Free Parking", 0, 0, 160, 140)
 free_parking.update_dicto(free_parking, 20)
 
 strand = my_property_class("Strand", 0, 1, width, 140, "firebrick1", 18, 220,
-                           90, 250, 700, 875, 1050, 150, 150, 110, "s",w,h)
+                           90, 250, 700, 875, 1050, 150, 150, 110, "s")
 strand.update_dicto(strand, 21)
 
 chance2 = my_property_class("Chance", 0, 2, width, 140)
 chance2.update_dicto(chance2, 22)
 
 fleet_street = my_property_class("fleet street", 0, 3, width, 140, "firebrick1", 18, 220,
-                                 90, 250, 700, 875, 1050, 150, 150, 110, "s",w,h)
+                                 90, 250, 700, 875, 1050, 150, 150, 110, "s")
 fleet_street.update_dicto(fleet_street, 23)
 
 trafalagar_square = my_property_class("trafalagar square", 0, 4, width, 140, "firebrick1", 20, 240,
-                                      100, 300, 750, 925, 1100, 150, 150, 120, "s",w,h)
+                                      100, 300, 750, 925, 1100, 150, 150, 120, "s")
 trafalagar_square.update_dicto(trafalagar_square, 24)
 
 fenchurch_st_station = my_property_class("fenchurch st station", 0, 5, width, 140)
 fenchurch_st_station.update_dicto(fenchurch_st_station, 25)
 
 licester_square = my_property_class("licester square", 0, 6, width, 140, "yellow", 22, 290,
-                                    110, 330, 800, 975, 1150, 150, 150, 130, "s",w,h)
+                                    110, 330, 800, 975, 1150, 150, 150, 130, "s")
 licester_square.update_dicto(licester_square, 26)
 
 coventry_street = my_property_class("coventry street", 0, 7, width, 140, "yellow", 22, 290,
-                                    110, 330, 800, 975, 1150, 150, 150, 130, "s",w,h)
+                                    110, 330, 800, 975, 1150, 150, 150, 130, "s")
 coventry_street.update_dicto(coventry_street, 27)
 
 water_works = my_property_class("water works", 0, 8, width, 140)
 water_works.update_dicto(water_works, 28)
 
 piccadilly = my_property_class("piccadilly", 0, 9, width, 140, "yellow", 24, 280,
-                               120, 360, 850, 1025, 1200, 150, 150, 140, "s",w,h)
+                               120, 360, 850, 1025, 1200, 150, 150, 140, "s")
 piccadilly.update_dicto(piccadilly, 29)
 
 go_to_jail = my_property_class("go_to_jail", 0, 10, 160, 140)
 go_to_jail.update_dicto(go_to_jail, 30)
 
 # right lane
-w = width / 3
-h = height
-
 regent_street = my_property_class("regent street", 1, 10, 160, height, "green", 26, 300,
-                                  130, 390, 900, 1100, 1275, 200, 200, 150, "w",w,h)
+                                  130, 390, 900, 1100, 1275, 200, 200, 150, "w")
 regent_street.update_dicto(regent_street, 31)
 
 oxford_street = my_property_class("oxford street", 2,10 , 160, height, "green", 26, 300,
-                                  130, 390, 900, 1100, 1275, 200, 200, 150, "w",w,h)
+                                  130, 390, 900, 1100, 1275, 200, 200, 150, "w")
 oxford_street.update_dicto(free_parking, 32)
 
 community_chest = my_property_class("community_chest", 3,10, 160, height)
 community_chest.update_dicto(community_chest, 33)
 
 bond_street = my_property_class("bond street", 4, 10, 160, height, "green", 28, 320,
-                                150, 450, 1000, 1200, 1400, 200, 200, 160, "w",w,h)
+                                150, 450, 1000, 1200, 1400, 200, 200, 160, "w")
 bond_street.update_dicto(bond_street, 34)
 
 liverpool_st_station = my_property_class("liverpool_st_station", 5, 10, 160, height)
@@ -237,36 +226,34 @@ chance3 = my_property_class("chance3", 6, 10, 160, height)
 chance3.update_dicto(chance3, 36)
 
 park_lane = my_property_class("park_lane", 7, 10, 160, height, "dark blue", 35, 350,
-                              175, 500, 1100, 1300, 1500, 200, 200, 175, "w",w,h)
+                              175, 500, 1100, 1300, 1500, 200, 200, 175, "w")
 park_lane.update_dicto(park_lane, 37)
 
 super_tax = my_property_class("super_tax", 8, 10, 160, height)
 super_tax.update_dicto(super_tax, 38)
 
 mayfair = my_property_class("mayfair", 9, 10, 160, height, "dark blue", 50, 400,
-                            200, 600, 1400, 1700, 2000, 200, 200, 200, "w",w,h)
+                            200, 600, 1400, 1700, 2000, 200, 200, 200, "w")
 mayfair.update_dicto(mayfair, 39)
 
-# lower lane
-w = width
-h = height / 2
+# lower lane\
 
 just_visiting = my_property_class("just_visting", 10, 0, 160, 140)
 just_visiting.update_dicto(just_visiting, 10)
 
 pentoville_road = my_property_class("pentoville road", 10, 1, width, 140, "light blue", 8, 120,
-                                    40, 100, 300, 450, 600, 50, 50, 60, "n",w,h)
+                                    40, 100, 300, 450, 600, 50, 50, 60, "n")
 pentoville_road.update_dicto(pentoville_road, 9)
 
 euston_road = my_property_class("euston road", 10, 2, width, 140, "light blue", 6, 100,
-                                30, 90, 270, 400, 550, 50, 50, 50, "n",w,h)
+                                30, 90, 270, 400, 550, 50, 50, 50, "n")
 euston_road.update_dicto(euston_road, 8)
 
 chance1 = my_property_class("chance", 10, 3, width, 140)
 chance1.update_dicto(chance1, 7)
 
 the_angel_islington = my_property_class("the angel islington", 10, 4, width, 140, "light blue", 6, 100,
-                                        30, 90, 270, 400, 550, 50, 50, 50, "n",w,h)
+                                        30, 90, 270, 400, 550, 50, 50, 50, "n")
 the_angel_islington.update_dicto(the_angel_islington, 6)
 
 kings_cross_station = my_property_class("kings cross station", 10, 5, width, 140)
@@ -276,14 +263,14 @@ income_tax = my_property_class("income tax!", 10, 6, width, 140)
 income_tax.update_dicto(income_tax, 4)
 
 white_chapal_road = my_property_class("white chapal road", 10, 7, width, 140, "brown", 4, 60,
-                                      20, 60, 180, 320, 450, 50, 50, 30, "n",w,h)
+                                      20, 60, 180, 320, 450, 50, 50, 30, "n")
 white_chapal_road.update_dicto(white_chapal_road, 3)
 
 community_chest1 = my_property_class("community chest", 10, 8, width, 140)
 community_chest1.update_dicto(community_chest1, 2)
 
 old_kent_road = my_property_class("old kent road", 10, 9, width, 140, "brown", 2, 60,
-                                  10, 30, 90, 160, 250, 50, 50, 30, "n",w,h)
+                                  10, 30, 90, 160, 250, 50, 50, 30, "n")
 old_kent_road.update_dicto(old_kent_road, 1)
 
 go_box = my_property_class("go box", 10, 10, 160, 140)
@@ -291,40 +278,37 @@ go_box.update_dicto(go_box, 0)
 
 # left lane
 
-w = width / 3
-h = height
-
 pall_mall = my_property_class("pall mall", 9, 0, 160, height, "pink", 10, 140,
-                              50, 150, 450, 625, 750, 100, 50 * 2, 70, "e",w,h)
+                              50, 150, 450, 625, 750, 100, 50 * 2, 70, "e")
 pall_mall.update_dicto(pall_mall, 11)
 
 electric_company = my_property_class("electric company", 8, 0, 160, height)
 electric_company.update_dicto(electric_company, 12)
 
 white_hall = my_property_class("white hall", 7, 0, 160, height, "pink", 10, 140,
-                               50, 150, 450, 625, 750, 100, 50 * 2, 70, "e",w,h)
+                               50, 150, 450, 625, 750, 100, 50 * 2, 70, "e")
 white_hall.update_dicto(white_hall, 13)
 
 northumber_ld_avenue = my_property_class("northumberl'd avenue", 6, 0, 160, height, "pink", 12, 160, 60,
-                                         180, 500, 700, 900, 100, 100, 80, "e",w,h)
+                                         180, 500, 700, 900, 100, 100, 80, "e")
 northumber_ld_avenue.update_dicto(northumber_ld_avenue, 14)
 
 marylebone_station = my_property_class("marltbone station",5 , 0, 160, height)
 marylebone_station.update_dicto(marylebone_station, 15)
 
 bow_street = my_property_class("bow street", 4, 0, 160, height, "orange", 14, 180,
-                               70, 200, 550, 750, 950, 100, 50 * 2, 90, "e",w,h)
+                               70, 200, 550, 750, 950, 100, 50 * 2, 90, "e")
 bow_street.update_dicto(bow_street, 16)
 
 community_chest2 = my_property_class("community chest", 3, 0, 160, height)
 community_chest2.update_dicto(community_chest2, 17)
 
 marlborough_street = my_property_class("marlborough street",2,0, 160, height, "orange", 14, 180,
-                                       70, 200, 550, 750, 950, 100, 50 * 2, 90, "e",w,h)
+                                       70, 200, 550, 750, 950, 100, 50 * 2, 90, "e")
 marlborough_street.update_dicto(marlborough_street, 18)
 
 vine_street = my_property_class("vine street", 1, 0, 160, height, "orange", 16, 200,
-                                80, 220, 600, 800, 1000, 100, 50 * 2, 100, "e",w,h)
+                                80, 220, 600, 800, 1000, 100, 50 * 2, 100, "e")
 vine_street.update_dicto(vine_street, 19)
 
 monopoly_dis = tk.Label(main_frame, text="Monopoly", bg="tomato1", fg="white")
